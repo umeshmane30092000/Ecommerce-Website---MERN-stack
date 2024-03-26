db79f8fee85fea123ac3f2152b6cb5f55d2661b8
Umesh <umeshmane575@gmail.com>
2024-03-26 20:00:50 +0530

    registration page is created

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { auth } from '../../firebase'
 
 const Register = () => {
-    return(
-        <>
-        <p> this is the register page</p>  
-        </>
+    const [email, setemail] = useState("");
+
+
+    const handleSubmit = () => {
+
+
+    }
+
+    const registerForm = () => <form onSubmit={handleSubmit}>
+        <input
+            type="email"
+            className="form-control"
+            value={email}
+            onChange={e => setemail(e.target.value)}
+            autoFocus
+        />
+
+        <button type="submit" className="btn btn-raised">Register</button>
+
+    </form>
+
+
+
+    return (
+        <div className="container p-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <h1>Register</h1>
+                    {registerForm()}
+                </div>
+            </div>
+        </div>
     )
 }
 
