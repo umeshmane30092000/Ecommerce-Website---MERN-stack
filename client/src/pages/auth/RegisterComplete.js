0ffc13460ed3006128dfaa7c185be14aada9cd82
Umesh <umeshmane575@gmail.com>
2024-03-27 18:48:55 +0530

    Registration page functinoality is complete now we will going to set up redux global state for user & token of that so that we can  acsesss it in the other page baceause we need inforamtion about the login user in the other page to task

--- client/src/pages/auth/RegisterComplete.js
+++ client/src/pages/auth/RegisterComplete.js
@@ -1,56 +1,91 @@
-import React , { useState ,useEffect }from 'react'
+import React, { useState, useEffect } from 'react'
 import { auth } from '../../firebase';
+import { toast } from 'react-toastify';
 
-const RegisterComplete = () => {
-    const [email , setemail] = useState("")
-    const [password , setPassword] = useState("");
+const RegisterComplete = ({ history }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
 
-  useEffect(() => {
+    useEffect(() => {
 
-    setemail(window.localStorage.getItem('emailForRegistration'))
+        setEmail(window.localStorage.getItem('emailForRegistration'))
 
-  },[])
+    }, [])
 
-  const handSubmit = (e) => {
-    e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!email || !password) {
+            toast.error("Email and password is required");
+            return;
+        }
 
-  }
+        if (password.length < 6) {
+            toast.error("Password must be at least 6 characters long");
+            return;
+        }
 
-   
+        try {
 
-  const completeRegistrationForm = () => <form onSubmit={handSubmit}>
-  <input 
-    type="email"
-    placeholder="Enter email"
-    className="form-control"
-    value={email}
-    disabled
-  />
-  <input
-    tyep="password"
-    placeholder="Enter password"
-    className="form-control"
-    value={password}
-    onChange={e => setPassword(e.target.value)}
-  />
- <br/>
-  <button type="submit" className="btn btn-raised">Registration Complete</button>
-     
-  </form>
+            const result = await auth.signInWithEmailLink(
+                email,
+                window.location.href
+            );
+            //   console.log("RESULT", result);
+            if (result.user.emailVerified) {
+                // remove user email fom local storage
+                window.localStorage.removeItem("emailForRegistration");
+                // get user id token
+                let user = auth.currentUser;
+                await user.updatePassword(password);
+                const idTokenResult = await user.getIdTokenResult();
+                // redux store
+                console.log("user", user, "idTokenResult", idTokenResult);
 
-  
-  return (
-    
-     <div className="container p-5">
-        <div className="row">
-            <div className="col-md-6 offset">
-                <h1>Register complete</h1>
-                {completeRegistrationForm()}
+                // redirect
+                history.push("/");
+
+            }
+
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        }
+    }
+
+    const completeRegistrationForm = () => (
+        <form onSubmit={handleSubmit}>
+            <input type="email"
+                className="form-control"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <input
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                autoFocus
+            />
+            <br />
+            <button type="submit" className="btn btn-raised">
+                Complete Registration
+            </button>
+        </form>
+    );
+
+    return (
+        <div className="container p-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <h4>Register Complete</h4>
+                    {completeRegistrationForm()}
+                </div>
             </div>
         </div>
-     </div>
-  )
-}
+    );
+};
 
-export default RegisterComplete
+export default RegisterComplete;
