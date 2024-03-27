5e6b49c7633b906da46aeb8ba78c26c2c8bc3e09
Umesh mane <102271773+umeshmane30092000@users.noreply.github.com>
2024-03-27 16:30:34 +0530

    Update firebase.js
    edited

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,20 +1,2 @@
-import * as firebase from "firebase";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyBLB2xZGOpNijga-cs_AlVC08kbFrvCtEQ",
-    authDomain: "e-commerce-mern-project.firebaseapp.com",
-    projectId: "e-commerce-mern-project",
-    storageBucket: "e-commerce-mern-project.appspot.com",
-    messagingSenderId: "472597429436",
-    appId: "1:472597429436:web:a3e2487371fe625f9f24b9"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-
-// export
-export const auth = firebase.auth();
-export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-
+// firebase
 
