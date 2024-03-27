dd949dad65fd949cebaef02bc685dcbb1b2f4d1e
Umesh <umeshmane575@gmail.com>
2024-03-27 10:08:07 +0530

    registration page functinality completed  sending email to user

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,12 +1,32 @@
 import React, { useState } from 'react';
 import { auth } from '../../firebase'
+import { toast, ToastContainer} from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
     const [email, setemail] = useState("");
 
 
-    const handleSubmit = () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
+        // to sned email using firebase 
+        const config = {
+            url: 'http://localhost:3000/register/complete',
+            handleCodeInApp: true,
+        };
+
+        await auth.sendSignInLinkToEmail(email, config);
+        toast.success(
+            `Email is sned to ${email} Click to complete registration`
+        );
+
+        // save the email in the local storage so that we can prepopulate 
+        // in the registration complete page
+        window.localStorage.setItem("emailForRegistration" , email);
+       
+        // clear the state after sumit the form
+        setemail("");
 
     }
 
@@ -30,6 +50,7 @@ const Register = () => {
             <div className="row">
                 <div className="col-md-6 offset-md-3">
                     <h1>Register</h1>
+                    <ToastContainer/>
                     {registerForm()}
                 </div>
             </div>
