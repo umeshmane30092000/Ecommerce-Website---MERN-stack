fed93e77bdb3cca60e287afa11ec4744488d3c59
Umesh <umeshmane575@gmail.com>
2024-04-02 11:28:55 +0530

    conditional redering is done when user is logedIn we have only shown dropudown with user email from the state if not i have shown login and register on screen

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react'
 import { Menu } from 'antd';
-import { AppstoreOutlined, SettingOutlined, UserOutlined, UserAddOutlined , LogoutOutlined } from '@ant-design/icons'
+import { AppstoreOutlined, SettingOutlined, UserOutlined, UserAddOutlined, LogoutOutlined } from '@ant-design/icons'
 import { Link } from 'react-router-dom';
 import firebase from 'firebase'
-import {useHistory} from 'react-router-dom'
-import { useDispatch} from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 
 const { SubMenu, Item } = Menu;
 
 const Header = () => {
 
   const [current, setCurrent] = useState("home");
-  let  dispatch = useDispatch();
+  let dispatch = useDispatch();
+  let { user } = useSelector((state) => ({ ...state }))
+
   let history = useHistory();
 
   const handleClick = (e) => {
@@ -37,20 +39,30 @@ const Header = () => {
         <Link to="/">Home</Link>
       </Item>
 
-      <Item key="register" icon={<UserAddOutlined />} className='float-right'>
-        <Link to="/register">Register</Link>
-      </Item>
+      {!user &&
+        <Item key="register" icon={<UserAddOutlined />} className='float-right'>
+          <Link to="/register">Register</Link>
+        </Item>
+      }
 
-      <Item key="login" icon={<UserOutlined />} className='float-right'>
-        <Link to="/login">Login</Link>
-      </Item>
+      {!user
+        && <Item key="login" icon={<UserOutlined />} className='float-right'>
+          <Link to="/login">Login</Link>
+        </Item>
+      }
 
-      <SubMenu icon={<SettingOutlined />} title="Username">
-        <Item key="setting:1">option1</Item>
-        <Item key="setting:2">option1</Item>
-        <Item icon={<LogoutOutlined/>} onClick={logout}>Logout</Item>
+      {user
+        &&
+        <SubMenu
+          icon={<SettingOutlined />}
+          title={user.email && user.email.split('@')[0]}
+          className='float-right' >
+          <Item key="setting:1">option1</Item>
+          <Item key="setting:2">option1</Item>
+          <Item icon={<LogoutOutlined />} onClick={logout}>Logout</Item>
 
-      </SubMenu>
+        </SubMenu>
+      }
 
     </Menu>
   )
