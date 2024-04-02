86f025cb417c4e2c289ed57cf009c199351b7a04
Umesh <umeshmane575@gmail.com>
2024-04-02 10:33:34 +0530

    Login with google is done using firebase with gooleAuthProvider and signwithpop finction also dispatch result in the redux

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,19 +1,63 @@
 import React, { useState } from 'react';
-import { auth } from "../../firebase";
+import { auth, googleAuthProvider } from "../../firebase";
 import { toast } from "react-toastify";
-import {Button} from 'antd'
-import {MailOutlined} from '@ant-design/icons'
+import { Button } from 'antd'
+import { GoogleOutlined, MailOutlined } from '@ant-design/icons'
+import { useDispatch } from 'react-redux'
 
 
-const Login = () => {
+const Login = ({ history }) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [loading, setLading] = useState(false);
+
+
+    let dispatch = useDispatch();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setLading(true);
+        try {
+            const result = await auth.signInWithEmailAndPassword(email, password);
+            const { user } = result;
+            const idTokenResult = await user.getIdTokenResult();
+            dispatch({
+                type: "LOGGED_IN_USER",
+                payload: {
+                    email: user.email,
+                    token: idTokenResult.token,
+                },
+            });
+            history.push("/");
 
-        console.table(email , password)
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message);
+            setLading(false);
 
+        }
+    }
+
+
+    const googleLogin = async () => {
+        auth.signInWithPopup(googleAuthProvider)
+            .then(async (result) => {
+                const { user } = result;
+                const idTokenResult = await user.getIdTokenResult();
+                dispatch({
+                    type: "LOGGED_IN_USER",
+                    payload: {
+                        email: user.email,
+                        token: idTokenResult.token,
+                    },
+                });
+                history.push("/");
+            })
+            .catch(err => {
+                console.log(err)
+                toast.error(err.message)
+            })
+            
 
     }
 
@@ -42,15 +86,15 @@ const Login = () => {
             </div>
             <br />
             <Button
-             onClick={handleSubmit}
-             type="primary"
-             className="mb-3"
-             block
-             shape='round'
-             icon={<MailOutlined/>}
-             size='large'
-             disabled={!email || password.length < 6}
-             >
+                onClick={handleSubmit}
+                type="primary"
+                className="mb-3"
+                block
+                shape='round'
+                icon={<MailOutlined />}
+                size='large'
+                disabled={!email || password.length < 6}
+            >
                 Login with Email/ Password
             </Button>
 
@@ -60,8 +104,18 @@ const Login = () => {
         <div className="container p-5">
             <div className="row">
                 <div className="col-md-6 offset-md-3">
-                    <h4>Login</h4>
+                    {loading ? (<h4 className='danger'>Loading</h4>) : <h4>Login</h4>}
                     {loginForm()}
+                    <Button
+                        onClick={googleLogin}
+                        type='danger'
+                        className="mb-3"
+                        block
+                        shape="round"
+                        icon={<GoogleOutlined />}
+                        size="large"
+                    >
+                        Login with Google</Button>
                 </div>
             </div>
         </div>
